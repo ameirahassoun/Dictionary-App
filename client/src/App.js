7d15db35f13f0a88aafcfc00915fdcf3baf739c4
ameirahassoun <afhassoun5@gmail.com>
2018-11-29 05:32:10 +0200

    add watch to react and import react app

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <h2>Dictionary</h2>
+          </header>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
