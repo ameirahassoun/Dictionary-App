f1bcaa02c8b96214d543f9eba8fbc1966f3e887c
ameirahassoun <afhassoun5@gmail.com>
2018-11-29 05:31:21 +0200

    add server and packeges

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname, '../react-client/build')));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(path.join(__dirname, '../react-client/build/index.html')))
+});
+
+const port = 3500;
+app.listen(port, () => {
+    console.log(`this is the port : ${port}`)
+});
