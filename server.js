44ec031c24392e08418ab92b60e1a952a4b6284d
ameirahassoun <afhassoun5@gmail.com>
2018-11-30 17:21:45 +0200

    edit post request and finishing server side

--- server.js
+++ server.js
@@ -2,29 +2,25 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-var Dictionary = require('oxford-dictionary-api')
+const Dictionary = require('oxford-dictionary-api');
 
 app.use(express.static(path.join(__dirname, './client/build')));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-// app.post('/test', (req, res) => {
-//     console.log(req.body);
-// })
+const app_id = "50f3da34"
+const app_key = "6c5c8f2126841ca1200443c8a1cd6142"
+const dict = new Dictionary(app_id, app_key)
 
-var app_id = "50f3da34"
-var app_key = "6c5c8f2126841ca1200443c8a1cd6142"
-var dict = new Dictionary(app_id, app_key)
-
-app.post('/definition',() => {
-    dict.find(req.body.elklmeh, function(error, data){
+app.post('/definition', (req, res) => {
+    const { e } = req.body;
+    dict.find(e, (error, data) => {
         if(error){
-            res.sendStatus(400)
-        }else{
-            res.send(data)
+            res.sendStatus(400);
         }
+        res.send(data);
     })
-})
+});
 
 app.get('/*', (req, res) => {
     res.sendFile(path.resolve(path.join(__dirname, './client/build/index.html')))
