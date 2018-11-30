be309fee021edd983aa50518c9da2c29bb1ee8c9
ameirahassoun <afhassoun5@gmail.com>
2018-11-30 17:29:10 +0200

    handling data recived after post request and filtring according to the category

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
+      elkelmeh: '',
+      eltrgmeh: [],
     }
   }
 
   onChange = e => {
-    console.log(e);
-  }
+    this.setState({
+      elkelmeh: e,
+    })
+  };
+
+  endari = () => {
+    const { elkelmeh } = this.state;
+    axios.post('/definition', {
+          e: elkelmeh
+      })
+      .then(({ data }) => {
+        let meaning = [];
+        let definition = [];
+        let bothCat = {}
+        data.results[0].lexicalEntries.forEach(element => {
+          element.entries.forEach(sens => {
+            sens.senses.forEach(def => {
+              meaning.push([element.lexicalCategory, def])
+            })
+          })
+        });
+        meaning.forEach(mean => {
+          if(!bothCat[mean[0]]){
+            bothCat[mean[0]] = mean[1]; 
+          }
+        })
+        definition.push(bothCat);
+        this.setState({
+          eltrgmeh: definition
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
+
   render() {
+    const { eltrgmeh } = this.state;
     return (
       <div>
         <div className="App">
@@ -22,6 +59,38 @@ class App extends Component {
             type='text'
             onChange={e => this.onChange(e.target.value)}
             />
+            <button
+              onClick={() => this.endari()}
+            >
+              button
+            </button>
+            <div>
+              {eltrgmeh.map(ele => {
+                return (<div>
+                  <h2>category : Noun</h2>
+                  <div>
+                    <h2>definition</h2>
+                  {ele.Noun.definitions[0]}
+                  </div>
+                      <div>
+                        <h2>example</h2>
+                        {ele.Noun.examples[0].text}
+                      </div>
+                    <div>
+                      <h2>category : Verb</h2>
+                  <div>
+                    <h2>definition</h2>
+                  {ele.Verb.definitions[0]}
+                  </div>
+                  <div>
+                    <h2>example</h2>
+                    {ele.Verb.examples[0].text}
+                  </div>
+                  </div>
+                  </div>
+                  )
+              })}
+            </div>
           </header>
         </div>
       </div>
